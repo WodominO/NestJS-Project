4f39793904778e32fdc40de69bce651fe1abb3a3
Dominik <dominik.wojcik@streamsage.io>
2023-07-18 14:31:02 +0200

    Dockerized app

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@app/app.module";
 
+const port = process.env.APP_PORT || 80;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
